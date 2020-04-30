ec52f1cd30fffe2d20a995f2e35bd415daf3aca4
Geerocktricks <geerockface4@gmail.com>
2020-04-30 13:41:26 -0700

    Made pages responsive

--- myspace/views.py
+++ myspace/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404,redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .models import Post,NewsLetterRecipients,Category, update_post_id_field_in_category_model
+from .models import Post,NewsLetterRecipients,Category, update_post_id_field_in_category_model,tags
 from .forms import NewsLetterForm
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 from .email import send_welcome_email
@@ -28,6 +28,7 @@ def post_list_by_category(request , category_slug):
 def post_list_view(request, category_slug=None):
     categories = Category.objects.all()
     post = Post.objects.filter(status='published')
+    tagz = tags.objects.all()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         post = post.filter(category=category)
@@ -57,10 +58,11 @@ def post_list_view(request, category_slug=None):
 
     twopercategory = {x: sample( categorypostdictionary[x], 2 ) for x in categorypostdictionary if len(list(set( categorypostdictionary[x]))) > 1}
 
-    return render(request, 'blog/post/list.html', {'posts': posts,"letterForm":form, 'recent': recent, 'categories': categories, 'post': post, 'twopercategory': twopercategory})
+    return render(request, 'blog/post/list.html', {'posts': posts,"letterForm":form, 'recent': recent, 'categories': categories, 'post': post, 'twopercategory': twopercategory, 'tagz':tagz})
 
 def post_detail_view(request, year, month, day, post, category_slug=None):
     categories = Category.objects.all()
+    recent = Post.objects.order_by('publish')[0:4]
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
@@ -75,7 +77,7 @@ def post_detail_view(request, year, month, day, post, category_slug=None):
             HttpResponseRedirect('post_detail_view')
     else:
         form = NewsLetterForm()
-    return render(request, 'blog/post/detail.html', {'post': post,"letterForm":form, 'categories': categories})
+    return render(request, 'blog/post/detail.html', {'post': post,"letterForm":form, 'categories': categories, 'recent': recent,})
 
 def search_results(request, category_slug=None):
     categories = Category.objects.all()
@@ -85,20 +87,20 @@ def search_results(request, category_slug=None):
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-        article = paginator.page(1)
+        post = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     if category_slug:
         posts = get_object_or_404(Category, slug=category_slug)
         post = post.filter(category=category)
+    
     if 'article' in request.GET and request.GET["article"]:
         search_term = request.GET.get("article")
         searched_articles = Post.search_by_title(search_term)
         message = f"{search_term}"
 
-        return render(request, 'blog/post/search.html',{"message":message,"articles": searched_articles, 'categories': categories, 'post': post,})
-
+        return render(request, 'blog/post/search.html',{"message":message,"articles": searched_articles, 'categories': categories, 'post': post,})   
     else:
         message = "You haven't searched for any term"
-    
-        return render(request, 'blog/post/search.html',{"message":message, 'categories': categories, 'post': post})
+
+        return render(request, 'blog/post/search.html',{"message":message, 'categories': categories, 'post': post ,})
